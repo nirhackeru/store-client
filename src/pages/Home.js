c108b70562cf7f2f2f4b3a63724ec57781a9cd96
nir lev ari <nirlevari@nirs-MacBook-Pro.local>
2021-04-19 19:50:52 +0300

    second update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <>
       <div className="jumbotron text-danger h1 font-weight-bold text-center">
-        <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
+        <Jumbotron text={["Welcome to", "React and Node", "Shop"]} />
       </div>
 
       <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
